adf40681f158263cdaf894b4095b4e359d741cdf
GoldFrosch <goldfrosch@daum.net>
2025-01-02 21:05:12 +0900

    feat: 플레이어 기본 이동 input action 추가

---
+++ Source/project_02/Characters/PlayerCharacter.cpp
@@ -0,0 +1,57 @@
+﻿#include "PlayerCharacter.h"
+
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Component/SurvivalComponent.h"
+
+
+APlayerCharacter::APlayerCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SurvivalComponent = CreateDefaultSubobject<USurvivalComponent>("Survival Component");
+}
+
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem =
+			ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(
+				GetLocalViewingPlayerController()->GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(DefaultMappingContext, 0);
+	}
+}
+
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<
+		UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveInputAction, ETriggerEvent::Triggered
+																			, this, &ThisClass::MoveTo);
+		EnhancedInputComponent->BindAction(LookInputAction, ETriggerEvent::Triggered
+																			, this, &ThisClass::Look);
+	}
+}
+
+void APlayerCharacter::MoveTo(const FInputActionValue& Value)
+{
+	const FVector VectorValue = Value.Get<FVector>();
+
+	UE_LOG(LogTemp, Display, TEXT("%f, %f, %f"), VectorValue.X, VectorValue.Y, VectorValue.Z);
+}
+
+void APlayerCharacter::Look(const FInputActionValue& Value)
+{
+	
+}
+
+
---
+++ Source/project_02/Characters/PlayerCharacter.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerCharacter.generated.h"
+
+class USurvivalComponent;
+class UInputComponent;
+class UInputMappingContext;
+class UInputAction;
+
+struct FInputActionValue;
+
+UCLASS()
+class PROJECT_02_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	APlayerCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = Survival, meta = (AllowPrivateAccess = true))
+	TObjectPtr<USurvivalComponent> SurvivalComponent;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input
+		, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input"
+		, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveInputAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input"
+		, meta = (AllowPrivateAccess = "true"))
+	UInputAction* LookInputAction;
+
+	UFUNCTION()
+	void MoveTo(const FInputActionValue& Value);
+
+	UFUNCTION()
+	void Look(const FInputActionValue& Value);
+};
