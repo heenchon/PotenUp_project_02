e17f7826ea19e88d115fc8b8b28bd4dbb02ac17d
GoldFrosch <goldfrosch@daum.net>
2025-01-06 18:52:34 +0900

    feat: Hook BP 구조 설계

---
+++ Source/project_02/Tool/HookRope.cpp
@@ -0,0 +1,25 @@
+﻿#include "HookRope.h"
+#include "CableComponent.h"
+
+AHookRope::AHookRope()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	DefaultRoot = CreateDefaultSubobject<USceneComponent>("DefaultSceneRoot");
+	SetRootComponent(DefaultRoot);
+	
+	Cable = CreateDefaultSubobject<UCableComponent>("Cable");
+	Cable->SetupAttachment(RootComponent);
+	
+	BodyMesh = CreateDefaultSubobject<UStaticMeshComponent>("Body Mesh");
+	BodyMesh->SetupAttachment(RootComponent);
+	
+	HookMesh = CreateDefaultSubobject<UStaticMeshComponent>("Hook Mesh");
+	HookMesh->SetupAttachment(RootComponent);
+}
+
+void AHookRope::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
---
+++ Source/project_02/Tool/HookRope.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HookRope.generated.h"
+
+class AHook;
+class UCableComponent;
+
+UCLASS()
+class PROJECT_02_API AHookRope : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AHookRope();
+
+	void ThrowHook();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "HookItem", meta = (AllowPrivateAccess = true))
+	TObjectPtr<UCableComponent> Cable;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "HookItem", meta = (AllowPrivateAccess = true))
+	TObjectPtr<UStaticMeshComponent> HookMesh;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "HookItem", meta = (AllowPrivateAccess = true))
+	TObjectPtr<UStaticMeshComponent> BodyMesh;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USceneComponent> DefaultRoot;
+};
