a0b519a36421389a0dc91e9a5797d3efe6f3ff4a
heenchon <heenchon@naver.com>
2025-01-11 15:52:17 +0900

    배 wrap 액터 제작

---
+++ Source/project_02/HY/Raft/RafaTop.cpp
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RafaTop.h"
+
+URafaTop::URafaTop()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void URafaTop::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	UE_LOG(LogTemp, Warning, TEXT("컴포넌트 틱"));
+	SetWorldLocation(GetComponentLocation()+WindDirection*DeltaTime*WindStrength*SailStrength);
+}
