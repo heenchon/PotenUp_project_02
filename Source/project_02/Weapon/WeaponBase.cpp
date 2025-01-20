058f0c6c308f826be2e6b973219e8da58cf28838
GoldFrosch <goldfrosch@daum.net>
2025-01-20 20:20:35 +0900

    feat: 상어 데미지 처리 추가

--- Source/project_02/Weapon/WeaponBase.cpp
+++ Source/project_02/Weapon/WeaponBase.cpp
@@ -39,6 +39,10 @@ void AWeaponBase::OnOverlapAttackCollision(UPrimitiveComponent* OverlappedCompon
 	int32 OtherBodyIndex, bool bFromSweep,
 	const FHitResult& SweepResult)
 {
+	if (OtherActor == GetOwner() || OtherActor == this)
+	{
+		return;
+	}
 	UGameplayStatics::ApplyDamage(OtherActor, Damage,
 		nullptr, GetOwner(),
 		UDamageType::StaticClass());
