fdc2a04a5e203a5650d980ba7198088c781c7166
heenchon <heenchon@naver.com>
2025-01-24 09:33:01 +0900

    라디오 생성

---
+++ Source/project_02/HY/Objects/Radio.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Radio.h"
+
+#include "Blueprint/UserWidget.h"
+
+
+ARadio::ARadio()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ARadio::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ARadio::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ARadio::Interact()
+{
+	Super::Interact();
+	EndingUI = CreateWidget(GetWorld(),EndingWidgetClass);
+	
+	if (EndingUI)
+	{
+		EndingUI->AddToViewport();
+	}
+}
+
---
+++ Source/project_02/HY/Objects/Radio.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlaceObjects.h"
+#include "Radio.generated.h"
+
+UCLASS()
+class PROJECT_02_API ARadio : public APlaceObjects
+{
+	GENERATED_BODY()
+
+public:
+	ARadio();
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class UUserWidget> EndingWidgetClass;
+	UPROPERTY()
+	UUserWidget* EndingUI;
+	
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+	virtual void Interact() override;
+};
