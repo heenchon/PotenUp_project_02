a2abded92e6055be10b0be45e8cc5cff0aa61423
heenchon <101083384+heenchon@users.noreply.github.com>
2025-01-30 00:27:36 +0900

    배 공격 진행

--- Source/project_02/HY/Shark/ESharkState.h
+++ Source/project_02/HY/Shark/ESharkState.h
@@ -4,9 +4,10 @@ UENUM(BlueprintType)
 enum class ESharkState : uint8
 {
 	Idle UMETA(DisplayName = "Idle"),
-	MoveToTarget UMETA(DisplayName = "MoveToTarget"),
+	MoveToRaft UMETA(DisplayName = "MoveToRaft"),
+	MoveToPlayer UMETA(DisplayName = "MoveToPlayer"),
 	AttackPlayer UMETA(DisplayName = "AttackPlayer"),
 	AttackRaft UMETA(DisplayName = "AttackRaft"),
 	RunAway UMETA(DisplayName = "RunAway"),
-	Turning UMETA(DisplayName = "Turning")
+	Turning UMETA(DisplayName = "Turning"),
 };
