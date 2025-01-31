79b1517d9e04137b7472d8387dda4e9c0889a20f
heenchon <101083384+heenchon@users.noreply.github.com>
2025-01-31 19:58:53 +0900

    상어 버그 수정

--- Source/project_02/HY/Shark/SharkAI.cpp
+++ Source/project_02/HY/Shark/SharkAI.cpp
@@ -93,7 +93,6 @@ void ASharkAI::OnMyBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor
 			UE_LOG(LogTemp,Display,TEXT("%d대 맞음"), CurHitCount);
 			if (CurHitCount >= 3)
 			{
-				CurHitCount = 0;
 				GetWorld()->GetTimerManager().ClearTimer(SharkTimerHandle);
 				TargetLocation = NewRunawayLocation(GetActorLocation(),MaxDist,MinDist);
 				NextState = ESharkState::RunAway;
@@ -170,6 +169,7 @@ void ASharkAI::MoveToRaft(float DeltaTime)
 	{
 		//타이머로 배 파괴 함수 실행.
 		GetWorld()->GetTimerManager().SetTimer(SharkTimerHandle,this,&ASharkAI::DamageFloor,FloorDestroyDuration,true);
+		CurHitCount = 0;
 		SetActorRotation(BiteRotation);
 		ChangeState(ESharkState::AttackRaft);
 	}
@@ -177,7 +177,7 @@ void ASharkAI::MoveToRaft(float DeltaTime)
 
 void ASharkAI::AttackRaft(float DeltaTime)
 {
-	SetActorLocation(Floor->GetActorLocation() - MouthPosition->GetComponentLocation()+ GetActorLocation());
+	SetActorLocation(Floor->GetActorLocation() - MouthPosition->GetComponentLocation()+ GetActorLocation() + BiteOffset);
 }
 
 void ASharkAI::DamageFloor()
@@ -187,9 +187,7 @@ void ASharkAI::DamageFloor()
 	if (Floor->GetDurability() <= 0.0f)
 	{
 		//배가 부서지면, Runaway
-		Floor->Destroy();
 		GetWorld()->GetTimerManager().ClearTimer(SharkTimerHandle);
-		
 		TargetLocation = NewRunawayLocation(GetActorLocation(),MaxDist,MinDist);
 		NextState = ESharkState::RunAway;
 		ChangeState(ESharkState::Turning);
@@ -305,13 +303,13 @@ ABuildingFloor* ASharkAI::GetFloor()
 	for (FVector& pos : positionArr)
 	{
 		ABuildingFloor* floor = Cast<ABuildingFloor>(raft->GetRaftBuildPointerData()[pos]);
-		if (floor->GetIsMain())
+		if (floor->GetIsMain() || floor->GetIsBroken())
 		{
 			continue;
 		}
 		if (IsAttackableFloor(positionArr, pos))
 		{
-			UE_LOG(LogTemp, Display, TEXT("공격할 판자 pos X: %f, Y: %f, Z: %f"), pos.X, pos.Y, pos.Z);
+			// UE_LOG(LogTemp, Display, TEXT("공격할 판자 pos X: %f, Y: %f, Z: %f"), pos.X, pos.Y, pos.Z);
 			return floor;
 		}
 	}
@@ -349,25 +347,29 @@ bool ASharkAI::IsAttackableFloor(const TArray<FVector>& positionArr, const FVect
 
 void ASharkAI::SetBiteRotation(const FVector& dir)
 {
-	if (dir == FVector(0, 1, 0)) 
-	{
-		// UE_LOG(LogTemp, Display, TEXT("동쪽이 비었음."));
-		BiteRotation = FRotator(30, -90, 0);
-	}
-	else if (dir == FVector(-1, 0, 0))
+	if (dir == FVector(-1, 0, 0))
 	{
 		// UE_LOG(LogTemp, Display, TEXT("남쪽이 비었음."));
 		BiteRotation = FRotator(30, 0, 0);
-	}
-	else if (dir == FVector(1, 0, 0))
-	{
-		// UE_LOG(LogTemp, Display, TEXT("북쪽이 비었음."));
-		BiteRotation = FRotator(30, 180, 0);
+		BiteOffset = FVector(-90, 0, 0);
 	}
 	else if (dir == FVector(0, -1, 0))
 	{
 		// UE_LOG(LogTemp, Display, TEXT("서쪽이 비었음."));
 		BiteRotation = FRotator(30, 90, 0);
+		BiteOffset = FVector(0, -90, 0);
+	}
+	else if (dir == FVector(0, 1, 0)) 
+	{
+		// UE_LOG(LogTemp, Display, TEXT("동쪽이 비었음."));
+		BiteRotation = FRotator(30, -90, 0);
+		BiteOffset = FVector(0, 90, 0);
+	}
+	else if (dir == FVector(1, 0, 0))
+	{
+		// UE_LOG(LogTemp, Display, TEXT("북쪽이 비었음."));
+		BiteRotation = FRotator(30, 180, 0);
+		BiteOffset = FVector(90, 0, 0);
 	}
 }
 
@@ -385,6 +387,8 @@ void ASharkAI::Died()
 void ASharkAI::Respawn()
 {
 	UE_LOG(LogTemp,Display,TEXT("상어 리스폰"));
+	GetWorld()->GetTimerManager().ClearTimer(SharkTimerHandle);
+	HealthPoint = 30.0f;
 	SetActorHiddenInGame(false);
 	SetActorEnableCollision(true);
 	TargetLocation = NewIdleLocation();
