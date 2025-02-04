2018fe613534cf2be6190443b6a9e769e347c621
GoldFrosch <goldfrosch@daum.net>
2025-02-04 13:09:07 +0900

    feat: 그릴 사운드 연동

--- Source/project_02/HY/Objects/Grill.cpp
+++ Source/project_02/HY/Objects/Grill.cpp
@@ -1,5 +1,6 @@
 ﻿#include "Grill.h"
 
+#include "Components/AudioComponent.h"
 #include "Kismet/GameplayStatics.h"
 #include "project_02/Characters/PlayerCharacter.h"
 #include "project_02/Characters/Component/InventoryComponent.h"
@@ -24,6 +25,8 @@ AGrill::AGrill()
 void AGrill::BeginPlay()
 {
 	Super::BeginPlay();
+	GrillingSoundComponent = UGameplayStatics::SpawnSoundAttached(GrillingSound, GetRootComponent());
+	GrillingSoundComponent->Deactivate();
 }
 
 void AGrill::Interact(AUsable_Item* input, int curItemIndex)
@@ -40,6 +43,8 @@ void AGrill::Interact(AUsable_Item* input, int curItemIndex)
 			UE_LOG(LogTemp, Warning, TEXT("물고기 굽기 시작."));
 			bIsCooking = true;
 			RawFoodMesh->SetVisibility(true);
+			GrillingSoundComponent->Activate();
+			GrillingSoundComponent->Play();
 			ProcessStart();
 		}
 	}
@@ -52,6 +57,7 @@ void AGrill::ProcessComplete()
 	AUsable_Item* fishCooked = GetWorld()->SpawnActor<AUsable_Item>(FishCookedTemp, FoodPoint->GetRelativeTransform());
 	fishCooked->AttachToActor(this,FAttachmentTransformRules::KeepRelativeTransform);
 	RawFoodMesh->SetVisibility(false);
+	GrillingSoundComponent->Deactivate();
 	bIsCooking = false;
 }
 
